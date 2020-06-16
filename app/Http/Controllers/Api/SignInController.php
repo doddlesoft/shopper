e67007b78a5fc199a47bd23ebc2c835241e1b162
Sam Bindoff <sdbindoff@gmail.com>
2020-06-16 18:33:00 +0100

    wip

--- app/Http/Controllers/Api/SignInController.php
+++ app/Http/Controllers/Api/SignInController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\SignInRequest;
 use App\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
